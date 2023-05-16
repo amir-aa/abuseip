6a12f7b47410ca1f713daa0818be1ee78ba9830a
amir-aa <aainfoira@gmail.com>
2023-05-16 13:21:17 +0330

    test completed

--- search.py
+++ search.py
@@ -1,6 +1,6 @@
 import mmap
 def scan_local(value):
-        with open(r'.\\hashdb.txt', 'rb', 0) as file:
+        with open(r'.\\badips.txt', 'rb', 0) as file:
             s = mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ)
             
             print(value)
