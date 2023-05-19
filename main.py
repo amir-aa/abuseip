6bfaf5eaae5540c25c676fde4830b143d0c4edcc
aainfoira@gmail.com <aainfoira@gmail.com>
2023-05-19 23:40:26 +0330

    log added

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-#!/usr/bin/python3
 from asgiref.wsgi import WsgiToAsgi
 from flask import Flask,jsonify,request,Response
 from token_1 import *
 from search import *
 app=Flask(__name__)
 asgi_app = WsgiToAsgi(app)
+LIMIT=4
 requests={}
 @app.route('/token')
 def token():
     k=generate_api_key()
     requests[k]=0
+    GetLog(request.remote_addr,k)
     return jsonify({'key':k})
 @app.route('/checkip/<ip>')
 def checkip(ip):
     try:
         if request.headers['apikey'] in requests:
-            #print("authorized!")
+            
             requests[request.headers['apikey']]+=1
+            if int(requests[request.headers['apikey']])>LIMIT:
+                return Response ("limit Exceeded! please take another token!",status=403)
             return jsonify(scan_local(ip))
         return  Response(
             "Unauthorized! You need an API token ",
--- token_1.py
+++ token_1.py
@@ -3,3 +3,7 @@ import secrets
 def generate_api_key(length=64):
     """Generate a secure API key"""
     return secrets.token_hex(length)
+def GetLog(ip,key):
+    with open('/var/log/ipabusekeylogs.log','a+') as filelog:
+        filelog.write(f"{ip}:{key}")
+        filelog.close()
