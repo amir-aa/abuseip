829ad103a627bc3da4e3d7ba4d384e9e8c1bbc7f
aainfoira@gmail.com <aainfoira@gmail.com>
2023-05-25 23:32:33 +0330

    before adding newroute

--- checkconnections.py
+++ checkconnections.py
@@ -1,13 +1,16 @@
 import os,time
 import threading
+from collections import Counter
 SLEEPTIME=50
 def checkSYNFLOOD():
     while 1:
         
         os.system("netstat -tna|grep SYN| awk '{print $5}' >/tmp/connections.txt")
         with open('/tmp/connections.txt','r') as f:
-            content=f.read()
-            content=content.strip()
+            content=f.readlines()
+            maliciouslist=[k for k,v in Counter(content).items() if v>3]
+            print(maliciouslist)
+            
         time.sleep(SLEEPTIME)
         
 if __name__=="__main__":
